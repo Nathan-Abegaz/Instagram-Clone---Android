6ca05eacfe00590fdf37c623db2ebfd8056cf605
Nathan <abegaznathan@gmail.com>
2021-03-17 23:30:54 -0700

    first commit

---
+++ app/src/main/java/com/example/instragramclone/ParseApplication.java
@@ -0,0 +1,11 @@
+package com.example.instragramclone;
+
+import android.app.Application;
+
+public class ParseApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
